115670f77985517272cd32123fac09887b709038
ssslavev <s.s.slavev@hotmail.com>
2019-05-19 11:53:38 +0300

    Change in loading handlebars template

--- public/index.js
+++ public/index.js
@@ -6,34 +6,31 @@ if (socket) {
 
     socket.on('notifications', (data) => {
 
-        let notifications = data.filter((notification) => notification.type != "bonus");
-        let notifCount = notifications.length;
+        tmplRequester.get('main')
+            .then((tmpl) => {
+                $('#wrapper').html(tmpl({ data: data }));
 
-        let html = document.getElementById('notif-template').innerHTML;
-        let template = Handlebars.compile(html);
-        let result = template({ data: data });
-        document.getElementById('wrapper').innerHTML = result;
-        $('#notif-badge').html(notifCount);
+                let notifications = data.filter((notification) => notification.type != "bonus");
+                let notifCount = notifications.length;
+                $('#notif-badge').html(notifCount);
 
+                $('#collapseExample div').each(function () {
 
-        $('#collapseExample div').each(function (idx, item) {
+                    if ($(this).data('info')) {
 
+                        setTimeout(() => {
 
-            if ($(this).data('info')) {
-
-                setTimeout(() => {
-
-                    $(this).fadeOut('slow');
-                    if ($(this).data('type') != 'bonus') {
-                        notifCount--;
+                            $(this).fadeOut('slow');
+                            if ($(this).data('type') != 'bonus') {
+                                notifCount--;
+                            }
+                            $('#notif-badge').html(notifCount);
+                        }, $(this).data('info'));
                     }
-                    $('#notif-badge').html(notifCount);
-                }, $(this).data('info'));
-            }
 
-        });
-
-    })
+                });
+            })
+    });
 
 } else {
     console.log('Problem with socket connection!....');
---
+++ public/scripts/tmplRequester.js
@@ -0,0 +1,20 @@
+let tmplRequester = (function () {
+    var handlebars = window.handlebars || window.Handlebars,
+        Handlebars = window.handlebars || window.Handlebars;
+
+    function get(name) {
+        var promise = new Promise((resolve, reject) => {
+            var url = `/templates/${name}.handlebars`;
+            //console.log(url);
+            $.get(url, function (templateHtml) {
+                var template = handlebars.compile(templateHtml);
+                resolve(template);
+            });
+        });
+        return promise;
+    }
+
+    return {
+        get
+    };
+}());
